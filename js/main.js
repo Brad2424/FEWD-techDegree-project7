73fd2e3f0433fac2ddc03395b0fe23655ce7cb47
Bradley Murchison <bradleymurchison@gmail.com>
2018-08-29 18:31:53 -0500

    refactored javascript for message user section

--- js/main.js
+++ js/main.js
@@ -14,41 +14,42 @@ alertButton.addEventListener('click', () => {
                     // ----- Event listner for MESSAGE USER section ----- //
                     // ---------------------------------------------------//
 
+const sentStatus = document.createElement("p");
+const parentDiv = document.querySelector('.messageUser');
+const message = document.querySelector('#message');
+const user = document.querySelector('#user');
+
 sendButton.addEventListener('click', (e) => {
-    const sendConfirmation = document.createElement("p");
-    sendConfirmation.setAttribute("id", "sentStatus");
-    const sentStatus = document.querySelector('.messageUser #sentStatus');
-
-    const parentDiv = document.querySelector('.messageUser');
-    const message = document.querySelector('#message');
-    const user = document.querySelector('#user');
-
+    // timeout functions for notice of a successully sent message
     let timeOut;
     function startTimeOut() {
-        timeOut = window.setTimeout(() => {sentStatus.remove()}, 3000);
+        timeOut = window.setTimeout(() => {
+            sentStatus.remove();
+            sentStatus.textContent = "";
+        }, 1000);
     };
     function stopTimeOut() {
         window.clearTimeout(timeOut);
     };
-
-    if (sentStatus !== null && sentStatus.textContent === "Message Successfully Sent"  && message.value === "" || sentStatus !== null && sentStatus.textContent === "Message Successfully Sent"  && user.value === "") {
+    // NOT the users first attempt 
+    if (sentStatus.textContent === "Message Successfully Sent"  && message.value === "" || sentStatus.textContent === "Message Successfully Sent"  && user.value === "") {
         sentStatus.textContent = "Error: No Message/User Entered";
         sentStatus.style.color = "red";
         stopTimeOut();
-    } else if (sentStatus !== null && sentStatus.textContent === "Error: No Message/User Entered" && message.value && user.value) {
+    } else if (sentStatus.textContent === "Error: No Message/User Entered" && message.value && user.value) {
         sentStatus.textContent = "Message Successfully Sent";
         sentStatus.style.color = "green";
         startTimeOut();
         message.value = "";
         user.value = "";
-
-    } else if(sentStatus === null && message.value === "" || sentStatus === null && user.value === "") {
-        parentDiv.insertBefore(sendConfirmation, e.target);
-        sendConfirmation.textContent = "Error: No Message/User Entered";
-        sendConfirmation.style.color = "red";
-    } else if(sentStatus === null && message.value && user.value) {
-        parentDiv.insertBefore(sendConfirmation, e.target);
-        sendConfirmation.textContent = "Message Successfully Sent";
+    // users first attempt 
+    } else if(sentStatus.textContent === "" && message.value === "" || sentStatus.textContent === "" && user.value === "") {
+        parentDiv.insertBefore(sentStatus, e.target);
+        sentStatus.textContent = "Error: No Message/User Entered";
+        sentStatus.style.color = "red";
+    } else if(sentStatus.textContent === "" && message.value && user.value) {
+        parentDiv.insertBefore(sentStatus, e.target);
+        sentStatus.textContent = "Message Successfully Sent";
         startTimeOut();
         message.value = "";
         user.value = "";
@@ -70,8 +71,12 @@ function handleClickEvent(e) {
     if (e.target.getAttribute("aria-checked") == "true") {
         switchCircle.style.left = "-21px";
         e.target.setAttribute("aria-checked", "false");
+        switchCircle.parentNode.style.background = 'rgb(255, 75, 75)';
+        switchCircle.parentNode.style.border = 'rgb(255, 75, 75) 1px solid';
     } else if (e.target.getAttribute("aria-checked") == "false") {
         switchCircle.style.left = "29px";
         e.target.setAttribute("aria-checked", "true");
+        switchCircle.parentNode.style.background = 'rgb(116, 119, 191)';
+        switchCircle.parentNode.style.border = 'rgb(116, 119, 191) 1px solid';
     }
 };
