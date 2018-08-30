a2c636ac01fad2a7d98232371de6138c994fe037
Bradley Murchison <bradleymurchison@gmail.com>
2018-08-30 15:27:13 -0500

    line graph option buttons added

--- js/charts.js
+++ js/charts.js
@@ -1,41 +1,74 @@
+// GRAPH SECTIONS //
+
 const traffic = document.getElementById("traffic");
 const dailyTraf = document.getElementById("dailyTraf");
 const mobileUsers = document.getElementById("mobileUsers");
 
-let myCharts = new Chart(traffic, {
-    type: 'line',
-    data: {
-        labels: ["January", "February", "March", "April", "May", "June", 'July', 'August', 'September', 'October', 'Novemebr', 'December'],
-        datasets: [{
-            // label: false,
-            data: [700, 1200, 1100, 1200, 2000, 1500, 1600, 1100, 1200, 1800, 1300, 1600],
-            backgroundColor: [
-                'rgba(116, 119, 191, 0.2)',
-            ],
-            borderColor: [
-                'rgba(116, 119, 191, 1)',
-            ],
-            borderWidth: 1,
-            lineTension: 0,
-            pointBackgroundColor: 'white',
-            pointRadius: '4',
-        }],
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        responsiveAnimationDuration: 0,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-}
-});
+// --------------------------------------------------
+// Function for creating all graphs
+// --------------------------------------------------
 
-myCharts += new Chart(dailyTraf, {
+const createLineChart = (chartElement, labels, data, yAxis) => {
+    new Chart(chartElement, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{ 
+                data: data,
+                borderColor: '#65619E',
+                borderWidth: 1,
+                backgroundColor: 'rgba(116, 119, 191, 0.2)',
+                borderColor: [
+                    'rgba(116, 119, 191, 1)',
+                ],
+                borderWidth: 1,
+                lineTension: 0,
+                pointBackgroundColor: 'white',
+                pointRadius: '4',
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: yAxis,
+                    gridLines: {
+                        offsetGridLines: true,
+                    }
+                }],
+            }
+        }
+    });
+};
+
+// ----------------------------------------------------
+// Event listner for GRAPH OPTION BUTTONS on LINE CHART
+// ----------------------------------------------------
+const weeklyButton = document.querySelector("button#weekly");
+const monthlyButton = document.querySelector("button#monthly");
+const quarterlyButton = document.querySelector("button#quarterly");
+const yearlyButton = document.querySelector("button#yearly");
+
+monthlyButton.addEventListener("click", createLineChart(traffic, ["January", "February", "March", "April", "May", "June", 'July', 'August', 'September', 'October', 'Novemebr', 'December'], [700, 1200, 1100, 1200, 2000, 1500, 1600, 1100, 1200, 1800, 1300, 1600], {beginAtZero:true}));
+
+weeklyButton.addEventListener("click", createLineChart(traffic, ["1", "2", "3", "4", "5", "6", '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'], [100, 150, 120, 130, 160, 180, 170, 190, 170, 180, 160, 190], {beginAtZero:true}));
+
+
+
+
+// createLineChart(traffic, ["January", "February", "March", "April", "May", "June", 'July', 'August', 'September', 'October', 'Novemebr', 'December'], [700, 1200, 1100, 1200, 2000, 1500, 1600, 1100, 1200, 1800, 1300, 1600], {beginAtZero:true});
+
+
+// ----------------------------------------------------
+// Creation of BAR GRAPH
+// ----------------------------------------------------
+
+new Chart(dailyTraf, {
     type: 'bar',
     data: {
         labels: ["Monday", 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
@@ -75,7 +108,10 @@ myCharts += new Chart(dailyTraf, {
     }
 });
 
-myCharts += new Chart(mobileUsers, {
+// ----------------------------------------------------
+// Creation of DOUGHNUT GRAPH
+// ----------------------------------------------------
+new Chart(mobileUsers, {
     type: 'doughnut',
     data: {
         labels: ["Phones", "Tablets", "Desktop"],
